b572964ad7a1a5fa469cc0f661f4bc3256c1e379
zhangfenglei <zhangfenglei@huawei.com>
2024-06-19 22:47:00 +0800

    TODO

---
+++ include/link.h
@@ -0,0 +1,7 @@
+#ifndef LINK_H
+#define LINK_H
+
+namespace link {
+}
+
+#endif
---
+++ lib/link.cpp
@@ -0,0 +1,17 @@
+#include "link.h"
+
+/*
+
+1. arrange/concatenate sections (.text; .rodata; .data; .bss)
+
+   each file
+     each section's offset
+
+2. symbols (global; weak)
+
+3. relocations
+*/
+
+namespace link {
+
+}
